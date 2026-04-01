44c99d862ae1864d9a933d4640fbee092b611314
Saad ullah khan <saadullahk127@gmail.com>
2026-04-01 19:58:43 +0500

    python assignment no 1

---
+++ Assignment_no_1.py
@@ -0,0 +1,23 @@
+#variable assignment
+
+#variables
+name = "saad" 
+age = 17
+gender = "male"
+city = "Karachi"
+is_student = True
+gpa = 3.8
+
+#print variables
+print("Name:", name)
+print("age:", age)
+print("gender:", gender)
+print("City:", city)
+print("Is student:", is_student)
+print("Gpa:", gpa)
+
+# print combined variables
+print("my name is", name, "and i am" , age , "years old. I live in ", city, "and i am a student. My GPA is:", gpa)
+
+# print using f-string
+print(f"My name is {name} and I am {age} years old and I live in {city}. I am a student and my GPA is {gpa}.")
